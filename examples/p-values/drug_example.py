cabd1351677b693c26bd07f9e8bcb7fb6e3c195c
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-04-08 12:44:59 +1200

    Nice colors

--- examples/p-values/drug_example.py
+++ examples/p-values/drug_example.py
@@ -16,11 +16,19 @@ x = np.arange(0, N+1)
 p = scipy.misc.comb(N, x)*0.75**x*(1.0 - 0.75)**(N - x)
 p /= p.sum()
 
+# P(x <= 31 | H0) and P(x >= 44 | H0)
+pval_left  = np.sum(p[x <= 31])
+pval_right = np.sum(p[x >= 44])
+
 # Bar plot
-width = 0.3
-plt.bar(x - 0.5*width, p, width=width, color="grey")
-plt.xlabel("$x$")
-plt.ylabel("$p(x | H_0)$")
+width = 0.5
+plt.bar(x - 0.5*width, p, width=width, color="LightGray")
+plt.hold(True)
+plt.bar(x - 0.5*width, p*(x <= 31), width=width, color="green")
+plt.bar(x - 0.5*width, p*(x >= 44), width=width, color="yellow")
+
+plt.xlabel("Number of successes, $x$")
+plt.ylabel("Probability given $H_0$, $p(x | H_0)$")
 plt.xlim([-0.5, N+0.5])
 plt.savefig("drug_example.pdf", bbox_inches="tight")
 
